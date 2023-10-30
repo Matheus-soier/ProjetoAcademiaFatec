123a5adb25ed54e81c71182a61f5a33f79a4bccd
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-10-30 13:15:48 -0300

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,12 @@
+
 const entrarMenu = document.querySelector('#entrar-menu');
 const sairMenu = document.querySelector('#sair-menu');
 const menuMobile = document.querySelector('.itens-menu-mobile');
-const body = document.body;
 
 entrarMenu.addEventListener('click', () => {
     menuMobile.classList.add('show-menu');
-    body.classList.add('no-scroll');
-    disableScroll();
 });
 
 sairMenu.addEventListener('click', () => {
     menuMobile.classList.remove('show-menu');
-    body.classList.remove('no-scroll');
-    enableScroll();
 });
-
-function disableScroll() {
-    body.style.overflow = 'hidden';
-}
-
-function enableScroll() {
-    body.style.overflow = 'auto';
-}
