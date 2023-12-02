8d0a010d86339d2e62d38547c289987212221030
Matheus Sampaio Soier <devmatheus.soier@gmail.com>
2023-12-02 16:40:59 -0300

    page login finalizada

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,23 @@ sairMenu.addEventListener('click', () => {
     menuMobile.classList.remove('show-menu');
 });
 
+const planosMob = document.querySelector('#planosMob').addEventListener('click', () => {
+    menuMobile.classList.remove('show-menu');
+});
+const atividadesMob = document.querySelector('#atividadesMob').addEventListener('click', () => {
+    menuMobile.classList.remove('show-menu');
+});
+
+const passArea = document.querySelector('#passwordArea');
+const btnPass = document.querySelector('#btnSenha');
+
+btnPass.addEventListener('click', () => {
+   if (passArea.type === 'password') {
+    passArea.setAttribute('type', 'text');
+    btnPass.classList.replace('fa-eye', 'fa-eye-slash')
+   } else {
+    passArea.setAttribute('type', 'password');
+    btnPass.classList.replace('fa-eye-slash', 'fa-eye');
+   }
+});
+
